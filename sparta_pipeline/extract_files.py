136e18d51b8a0fa9b3a019db9840333d4311f01a
Adam <75036521+Adam0J@users.noreply.github.com>
2021-04-09 10:40:36 +0100

    User story 1 completed, made extract_csv function.

---
+++ sparta_pipeline/extract_files.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+import pandas as pd
+
+
+s3 = boto3.client('s3')
+bucket_name = 'data20-final-project'
+bucket_contents = s3.list_objects_v2(Bucket=bucket_name)
+keys = [file['Key'] for file in bucket_contents['Contents']]
+
+
+def extract_csv():
+    csv_keys = []
+    csv_dfs = []
+    for key in keys:
+        if 'Academy' in key:
+            csv_keys.append(key)
+
+    for key in csv_keys:
+        s3_object = s3.get_object(
+            Bucket=bucket_name,
+            Key=key)
+        strbody = s3_object['Body']
+        initial_df = pd.read_csv(strbody)
+        csv_dfs.append(initial_df)
+    
+    return csv_dfs
+
+
+test = extract_csv()
+print(test)
+
