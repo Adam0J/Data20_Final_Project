0761d5b1686c7fcccaa3c5e08d053e8b772c9891
Adam <75036521+Adam0J@users.noreply.github.com>
2021-04-12 13:59:26 +0100

    New create_student_info.

--- sparta_pipeline/creating_tables.py
+++ sparta_pipeline/creating_tables.py
@@ -17,10 +17,11 @@ driver = 'ODBC+Driver+17+for+SQL+Server'
 engine = create_engine(f"mssql+pyodbc://{user}:{password}@{server}/{database}?driver={driver}")
 
 connection = engine.connect()
+meta = MetaData()
 
 
 def create_student_information():
-    meta = MetaData()
+
     students_information = Table(
         'students_information', meta,
         Column('id', Integer, primary_key=True),
@@ -28,12 +29,10 @@ def create_student_information():
         Column('date', Date),
         Column('self_development', Boolean),
         Column('geo_flex', Boolean),
-        Column('finanical_support_self', Boolean),
+        Column('financial_support_self', Boolean),
         Column('results', Boolean),
         Column('course_id', Integer),
-        Column('course_code_id', Integer)
+        Column('course_code_id', Integer, ForeignKey("course_codes.course_id"))
     )
     meta.create_all(engine)
 
-
-create_student_information()
