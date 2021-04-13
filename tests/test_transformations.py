9bf43d0ef4daa5a24698609da9fb0a468786b42f
Victor Novoa <victorhelionm@gmail.com>
2021-04-13 15:50:33 +0100

    transformation tests

---
+++ tests/test_transformations.py
@@ -0,0 +1,24 @@
+from sparta_pipeline import transformations
+import unittest
+
+class TransformationTests(unittest.TestCase):
+
+
+    def test_convert_si(self):
+        pass
+
+
+    def test_convert_scores(self):
+        pass
+    
+
+    def test_convert_pi(self):
+        pass
+
+
+    def test_convert_weeks(self):
+        pass
+
+
+    def test_convert_courses(self):
+        pass
