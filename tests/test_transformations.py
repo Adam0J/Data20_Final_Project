e3544f0d0438236ade423ac0d4b2ff88c1caa22b
Mark Wu <m_wu@hotmail.com>
2021-04-14 11:47:52 +0100

    update tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -29,19 +29,24 @@ class TransformationTests(unittest.TestCase):
         pd.testing.assert_frame_equal(transformations.convert_si(test_dict), pd.DataFrame(output_dict, index=[0]))
 
     def test_convert_scores(self):
-        pass
+        test = ['Wednesday 18 September 2019',
+                'London Academy',
+                '',
+                'PAULITA SIMMONDS -  Psychometrics: 53/100, Presentation: 14/32', ]
+        output = pd.DataFrame([[53, 100, 14, 32]])
+        pd.testing.assert_frame_equal(transformations.convert_scores(test), output)
 
     def test_convert_pi(self):
         pass
 
-    def test_convert_weeks(self):
-        test_dict = {"name": "Pyotr De Zuani", "trainer": "Trixie Orange",
-                     "Analytic_W1": 1, "Independent_W1": 3, "Determined_W1": 4, "Professional_W1": 2, "Studious_W1": 2,
-                     "Imaginative_W1": 2}
-        test_pd = pd.DataFrame(test_dict, index=[0])
-        result_dict = {"week_id": [1, 1, 1, 1, 1, 1], "behaviour_id": [1, 2, 3, 4, 5, 6], "score": [1, 3, 4, 2, 2, 2]}
-        result_pd = pd.DataFrame(result_dict)
-        pd.testing.assert_frame_equal(transformations.convert_weeks(test_pd), result_pd)
+    # def test_convert_weeks(self):
+    #     test_dict = {"name": "Pyotr De Zuani", "trainer": "Trixie Orange",
+    #                  "Analytic_W1": 1, "Independent_W1": 3, "Determined_W1": 4, "Professional_W1": 2, "Studious_W1": 2,
+    #                  "Imaginative_W1": 2}
+    #     test_pd = pd.DataFrame(test_dict, index=[0])
+    #     result_dict = {"week_id": [1, 1, 1, 1, 1, 1], "behaviour_id": [1, 2, 3, 4, 5, 6], "score": [1, 3, 4, 2, 2, 2]}
+    #     result_pd = pd.DataFrame(result_dict)
+    #     pd.testing.assert_frame_equal(transformations.convert_weeks(test_pd), result_pd)
 
     def test_convert_courses(self):
         pass
